2988b57729ca7947f1cd6e26090d251a77005c19
Aaron Brooks <aaron.brooks@viasat.com>
2016-07-10 15:07:57 -0500

    Bump clojure to 1.9.0-alpha9 for more recent clojure.spec.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha5"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha9"]
                  [org.clojure/test.check "0.9.0"]
                  [datascript "0.15.0"]
                  [lein-voom "0.1.0-20160311_203101-g259fbfc"]])
